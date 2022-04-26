776bb71349a5ca6a48c5bb1b7438e99f4f3575a1
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-26 10:05:32 -0300

    Working with MongoDB

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,33 @@
+//dependencies
+import { MongoClient } from 'mongodb';
+
+// API ROUTE:
+// POST /api/new-meetup
+
+async function handler(req, res) {
+	try {
+		if (req.method === 'POST') {
+			const data = req.body;
+			const { title, image, address, description } = data; //getting info to be sent to my db
+
+			//using mongodb
+			await MongoClient.connect(
+				'mongodb+srv://cynthia:DDnXCNLURPtWBGtV@cluster0.eusm6.mongodb.net/meetups?retryWrites=true&w=majority'
+			);
+			//creating db and collection
+			const db = client.db();
+			const meetupsCollection = db.collection('meetups');
+			//creating a document
+			const result = await meetupsCollection.insertOne(data);
+			console.log(result);
+			//closing db connection
+			client.close();
+
+			res.status(201).json({ message: 'Meetup inserted' });
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+}
+
+export default handler;
